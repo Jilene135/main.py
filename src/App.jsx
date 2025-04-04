d4626ea8eea718c4acbdbb9c977b07283dfa416a
Jilene135 <jilene.basigsig@accenture.com>
2025-04-05 01:15:43 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,6 @@ const siteProps = {
   title: "My Awesome Web App",
   description: "This is a web app that does amazing things.",
   author: "Jilene",
-  Job: Software Developer
+  job: "Software Developer",
   // Add any other properties you need
 };
